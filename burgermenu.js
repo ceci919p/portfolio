57a43bef8198f808060f163f5d62e469637adf0e
ceci919p <ceci919p@stud.kea.dk>
2021-06-09 22:48:34 +0200

    logo toggle with hidden added

--- burgermenu.js
+++ burgermenu.js
@@ -14,8 +14,10 @@ function toggleMenu() {
   if (erSkjult == true) {
     document.querySelector("#menuknap").textContent = "☰";
     document.querySelector("#nav_baggrund").classList.remove("nav_style");
+    document.querySelector(".logo").classList.remove("hidden");
   } else {
     document.querySelector("#menuknap").textContent = "X";
     document.querySelector("#nav_baggrund").classList.add("nav_style");
+    document.querySelector(".logo").classList.add("hidden");
   }
 }
